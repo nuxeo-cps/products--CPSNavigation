bcc2fb06323506b708baeb786083bf8b96a7b8a9
bdelbosc <devnull@localhost>
2004-01-19 09:03:30 +0000

    root_uid is required

--- skins/getNavigation.py
+++ skins/getNavigation.py
@@ -1,4 +1,4 @@
-##parameters=finder='cps', root_uid=None, REQUEST=None
+##parameters=finder, root_uid, REQUEST=None
 # $Id$
 from Products.CPSNavigation.ConfNavigation import ConfNavigation
 from Products.CPSNavigation.ZODBNavigation import ZODBNavigation
@@ -9,8 +9,6 @@ from Products.CPSNavigation.LDAPDirectoryNavigation import \
 b_start = REQUEST.get('b_start', 0)
 
 if finder == 'cps':
-    if not root_uid:
-        root_uid = 'sections'
     current_uid = REQUEST.get('current_uid')
     if not current_uid:
         current_uid = root_uid
@@ -45,13 +43,19 @@ elif finder == 'zodb':
                          batch_start=b_start,
                          )
 elif finder == 'ldap':
-    root_uid = 'ou=culture,o=gouv,c=fr'
+    current_uid = REQUEST.get('current_uid')
+
+    dir_name = root_uid
+    dir = getattr(context.portal_directories, dir_name)
+    root_uid = dir.ldap_base
+    if not current_uid:
+        current_uid = root_uid
     current_uid = REQUEST.get('current_uid', root_uid)
     nav = LDAPDirectoryNavigation(
         root_uid=root_uid,
         current_uid=current_uid,
         context=context,
-        dir_name='ldap_ou',
+        dir_name=dir_name,
         # include_root=0,
         batch_size=5,
         batch_start=b_start,
@@ -75,12 +79,3 @@ contents=leaf_4|leaf_5
 tree = nav.getTree()
 listing, listing_info, batch_info = nav.getListing()
 return tree, listing, batch_info, listing_info
-
-
-## a goal:
-## nav = Navigation(root_uid=portal, current=context, include_root=0,
-##                  filter = ('no_nodes', 'no_hidden', 'published')
-##                  sort = ('state:asc', 'date:desc'),
-##                  batch = {'size': 12, 'b_start':3},
-##                            )
-
