f2cd97a5ca7f1473f3505f973d3434d02e9fb877
bdelbosc <devnull@localhost>
2004-08-17 16:08:07 +0000

    creation of catalog nav select widget

---
+++ skins/cpsnavigation_default/getCPSNavigationWidgets.py
@@ -0,0 +1,16 @@
+##parameters=
+#$Id$
+
+widgets = {
+        'CPS Catalog Navigation Select Widget': {
+            'type': 'CPS Catalog Navigation Select Widget Type',
+            'data': {},
+            },
+# XXX not yet impl
+#        'CPS Catalog Navigation MultiSelect Widget': {
+#            'type': 'CPS Catalog Navigation MultiSelect Widget Type',
+#            'data': {},
+#            },
+        }
+
+return widgets
--- skins/cpsnavigation_default/getCatalogNavigationSelect.py
+++ skins/cpsnavigation_default/getCatalogNavigationSelect.py
@@ -1,6 +1,6 @@
-##parameters=REQUEST, SESSION_KEY='CPSNAVIGATION'
+##parameters=REQUEST, SESSION_KEY='CATALOGNAVIGATION'
 # $Id$
-from Products.CPSNavigation.CPSNavigation import CPSNavigation
+from Products.CPSNavigation.CatalogNavigation import CatalogNavigation
 
 TREE_PREF_SESSION_KEY = '%s_TREE_PREF' % SESSION_KEY
 SEARCH_PREF_SESSION_KEY = '%s_SEARCH_PREF' % SESSION_KEY
@@ -15,10 +15,13 @@ res = {'display_mode': 'tree',
        'rendered_search_form': '',
        'rendered_option_form': '',
        'status_form': '',
+       'title': '',
        }
 
 # manage form action
 form = REQUEST.form
+ltool = context.portal_layouts
+
 if form.has_key('option_submit'):
     option_form = form
 else:
@@ -46,22 +49,36 @@ option_pref = REQUEST.SESSION.get(OPTION_PREF_SESSION_KEY, {})
 
 # process navigation option
 save_tree_pref = 0
-if form.has_key('current_uid'):
-    tree_pref['current_uid'] = form.get('current_uid')
-    save_tree_pref = 1
-if form.has_key('display_mode'):
-    tree_pref['display_mode'] = form.get('display_mode')
-    save_tree_pref = 1
+prefs = ('widget_id', 'layout_id', 'input_id',
+         'current_uid', 'display_mode')
+for pref in prefs:
+    if form.has_key(pref):
+        tree_pref[pref] = form.get(pref)
+        save_tree_pref = 1
 if save_tree_pref:
     REQUEST.SESSION[TREE_PREF_SESSION_KEY] = tree_pref
+input_id = tree_pref.get('input_id')
+widget_id = tree_pref.get('widget_id')
+layout_id = tree_pref.get('layout_id')
 current_uid = tree_pref.get('current_uid')
 display_mode = tree_pref.get('display_mode')
 
-ltool = context.portal_layouts
+# get the widget
+widget = ltool[layout_id][widget_id]
+
+if not widget.is_editable_option and display_mode == 'option':
+    display_mode = None # prevent option edition
+res['title'] = widget.popup_title
+res['description'] = widget.popup_description
+res['view_macro_path'] = widget.view_macro_path
+res['popup_view_macro_path'] = widget.popup_view_macro_path
+res['is_editable_option'] = widget.is_editable_option
+res['input_id'] = input_id
+
 # process option form
 if not option_pref or display_mode == 'option':
     (res['rendered_option_form'], status, ds) = ltool.renderLayout(
-        layout_id='navigation_option', schema_id='navigation_option',
+        layout_id=widget.layout_option, schema_id=widget.schema_option,
         context=context, mapping=option_form, ob=option_pref)
     res['status_form'] = status
     if status != 'invalid':
@@ -81,7 +98,7 @@ if display_mode == 'option':
 # process search form
 if display_mode == 'search':
     (res['rendered_search_form'], status, ds) = ltool.renderLayout(
-        layout_id='navigation_search', schema_id='navigation_search',
+        layout_id=widget.layout_search, schema_id=widget.schema_search,
         context=context, mapping=search_form, ob=search_pref)
     res['status_form'] = status
     if status != 'invalid':
@@ -89,6 +106,7 @@ if display_mode == 'search':
 
 # build nav options
 kw = option_pref.copy()
+#kw['root_uid'] = 'workspaces'
 
 kw.update({'display_mode': display_mode,
            'current_uid': current_uid,
@@ -106,12 +124,11 @@ if display_mode == 'search':
         kw['query'] = query
 
 # nav process init
-nav = CPSNavigation(**kw)
-
+nav = CatalogNavigation(**kw)
 if display_mode == 'tree':
     res['tree'] = nav.getTree()
 
-#(res['listing'], res['listing_info'],
-# res['listing_batch_info']) = nav.getListing()
+(res['listing'], res['listing_info'],
+ res['listing_batch_info']) = nav.getListing()
 
 return res
